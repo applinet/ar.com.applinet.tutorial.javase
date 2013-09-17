6fb33af7528201f5bd6d67ef9aa6b78d45b013f8
Rodrigfer <Rodrigfer@DEV0PC03.seguros.local>
2013-09-17 12:30:00 -0300

    Agregado de Paquetes

---
+++ ProyectoIntegrador/src/Clase2_conceptosbasicos/clase2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clase2_conceptosbasicos;
+
+/**
+ *
+ * @author educacionit
+ */
+public class EjemploVisibilidadDeVariable {
+
+    public static void main(String[] args) {
+        boolean llueve = true;
+        int x = 10;
+        if (llueve) {
+        }
+        System.out.println("" + x);
+
+    }
+}
